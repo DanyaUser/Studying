5b76e8c6d45c6a6860e014af55330cd761d658d4
Danya <vvv2310@mail.ru>
2022-05-12 23:42:31 +0300

    third step with cases

--- main.js
+++ main.js
@@ -19,7 +19,28 @@ const personalMovieDB = {
 for(let i = 0; i <2; i++){
     const a = prompt('Один из последних просмотренных фильмов', ''),
        b = prompt('На сколько оцените его?', '');
-       personalMovieDB.movies[a] = b;
+
+       if(a != null && b != null && a != '' && b != '' && a.length < 50){ //проверяем все условия совпадают
+            personalMovieDB.movies[a] = b;
+            console.log('Done!');
+       }
+       else{
+           console.log('Error');
+           i--; //если нет, то возвращаемся к пред. итерации цикла
+       }
+}
+
+if (personalMovieDB.count < 10){
+    console.log("Просмотрено довольно мало фильмов");
+}
+else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+}
+else if(personalMovieDB.count >= 30){
+    console.log("Вы киноман");
+}
+else{
+    console.log("Ошибка!");
 }
 
 
